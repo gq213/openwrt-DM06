f023322602f6e8cda3661f38543d86ba2b374d88
gaoqiang <gaoqiang1211@gmail.com>
2016-06-01 17:56:13 +0800

    add dm06.

---
+++ target/linux/ramips/mt7688/profiles/dm06.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2011 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/DM06-profile
+	NAME:=dm06 iot board
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci
+endef
+
+define Profile/DM06-profile/Description
+	dm06 base packages.
+endef
+$(eval $(call Profile,DM06-profile))
