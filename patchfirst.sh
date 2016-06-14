1c855d995e4c5998a58decbe713e9bf8b56efe63
gaoqiang <gaoqiang1211@gmail.com>
2016-06-14 17:14:14 +0800

    modify patch script.

--- patchfirst.sh
+++ patchfirst.sh
@@ -9,25 +9,57 @@ fi
 
 echo "patching..."
 
-if [ ! -d $dir/feeds/packages/sound/shairport-sync/patches ]
-then
-	echo "creat shairport-sync/patches!!!"
-	mkdir -p $dir/feeds/packages/sound/shairport-sync/patches
+src_dir="$dir/target/linux/ramips/gq213-patches"
+src_file=(
+"$src_dir/shairport-sync/001-modify_snd_mmap.patch"
+"$src_dir/gst1-plugins-good/Makefile"
+"$src_dir/gst1-plugins-base/100-modify_snd_mmap.patch"
+"$src_dir/gst1-plugins-base/Makefile"
+"$src_dir/gst1-plugins-bad/Makefile"
+"$src_dir/faad2/001-modify_mult.patch"
+)
+
+dst_dir=(
+"$dir/feeds/packages/sound/shairport-sync/patches"
+"$dir/feeds/packages/multimedia/gst1-plugins-good"
+"$dir/feeds/packages/multimedia/gst1-plugins-base/patches"
+"$dir/feeds/packages/multimedia/gst1-plugins-base"
+"$dir/feeds/packages/multimedia/gst1-plugins-bad"
+"$dir/feeds/packages/libs/faad2/patches"
+)
+dst_file=(
+"/001-modify_snd_mmap.patch"
+"/Makefile"
+"/100-modify_snd_mmap.patch"
+"/Makefile"
+"/Makefile"
+"/001-modify_mult.patch"
+)
+
+for ((i=0; i<${#src_file[@]}; i++))
+do
+	files=${dst_dir[$i]}${dst_file[$i]}
+#	echo ${src_file[$i]}
+#	echo ${dst_dir[$i]}
+#	echo ${dst_file[$i]}
+	echo $files
+
+if [ -f $files ]; then
+	echo "compare..."
+	if [ ${src_file[$i]} -nt $files ]; then
+		echo "overwrite..."
+		cp -a ${src_file[$i]} $files
+	fi
+else
+	if [ ! -d ${dst_dir[$i]} ]; then
+		echo "creat dir..."
+		mkdir -p ${dst_dir[$i]}
+	fi
+
+	echo "copy..."
+	cp -a ${src_file[$i]} $files
 fi
-cp -rf $dir/target/linux/ramips/gq213-patches/shairport-sync/* $dir/feeds/packages/sound/shairport-sync/patches
 
-cp -rf $dir/target/linux/ramips/gq213-patches/gst1-plugins-good/* $dir/feeds/packages/multimedia/gst1-plugins-good
-
-cp -rf $dir/target/linux/ramips/gq213-patches/gst1-plugins-base/*.patch $dir/feeds/packages/multimedia/gst1-plugins-base/patches
-cp -rf $dir/target/linux/ramips/gq213-patches/gst1-plugins-base/Makefile $dir/feeds/packages/multimedia/gst1-plugins-base
-
-cp -rf $dir/target/linux/ramips/gq213-patches/gst1-plugins-bad/* $dir/feeds/packages/multimedia/gst1-plugins-bad
-
-if [ ! -d $dir/feeds/packages/libs/faad2/patches ]
-then
-	echo "creat faad2/patches!!!"
-	mkdir -p $dir/feeds/packages/libs/faad2/patches
-fi
-cp -rf $dir/target/linux/ramips/gq213-patches/faad2/* $dir/feeds/packages/libs/faad2/patches
+done
 
 echo "done,exit..."
