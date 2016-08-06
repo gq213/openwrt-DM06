41db13fefb6a966cca6185507f72a9c9969d7f60
gaoqiang <gaoqiang1211@gmail.com>
2016-08-07 00:12:46 +0800

    for some reason, only enable DLNA in LAN.

--- project/customfeed/platinum/files/dlna.sh
+++ project/customfeed/platinum/files/dlna.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
 NAME="$(uci get wireless.ap.ssid)"
-echo "start dlna daemon ($NAME)"
-FileMediaServerTest -f "$NAME-DMS" /tmp &
-MediaRendererTest -f "$NAME-L" &
 
-while true
-do
-        if [ -f "/tmp/DmrStart" ]; then
-                echo "daemon, start dmr..."
-                MediaRendererTest -f "$NAME-W" &
-                rm -rf /tmp/DmrStart
-        fi     
-               
-        sleep 3
-done
+FileMediaServerTest -f "$NAME-DMS" /tmp &
+MediaRendererTest -f "$NAME-L" >/dev/console 2>&1 &
